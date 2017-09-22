b3ddda6ef755fd2b9be81fbeccb5360eaef6f144
sakura-fly <sourcesakura@sina.com>
2017-09-22 09:53:29 +0800

    精简了算法

--- src/Ttet.java
+++ src/Ttet.java
@@ -3,22 +3,21 @@ public class Ttet {
 	public static void main(String[] args) {
 		printNum(15);
 	}
-	public static void printNum(int num){
-		
-		for(int i = 1; i < num; i++){
+
+	public static void printNum(int num) {
+
+		for (int i = 1; i < num; i++) {
 			int sum = i;
-			for (int j = i + 1; j <= num; j++) {
+			for (int j = i + 1; sum <= num; j++) {
 				sum += j;
-				if(sum > num){
-					break;//退出本次循环，开始下一次循环
-				} if(sum == num){
+				if (sum == num) {
 					String res = num + "=";
 					for (int k = i; k < j; k++) {
 						res += k + "+";
 					}
 					res += j;
 					System.out.println(res);
-					break;//退出本次循环，开始下一次循环
+					break;// 退出本次循环，开始下一次循环
 				}
 			}
 		}
